bad0bb3b14bd6037088af26a0241b7e98fc429dc
tjc01 <coveney279@gmail.com>
2025-07-12 12:12:18 +0100

    Address Bus refactored

--- src/addressBus/addressBus.h
+++ src/addressBus/addressBus.h
@@ -5,6 +5,13 @@
 #include "../cartridge/cartridge.h"
 
 class AddressBus{
+    struct Proxy{
+        AddressBus& mem;
+        u16 address;
+        Proxy(AddressBus& _mem, u16 _address) : mem{_mem}, address{_address} {}
+        operator u8(){return mem.read(address);}
+        void operator=(u8 value) { mem.write(address, value); }
+    };
 
 public:
     Cartridge& cartridge;
@@ -15,8 +22,12 @@ public:
     Array<constants::hRamSize> hRam;
     AddressBus(Cartridge& cart)
         : cartridge(cart), vram{}, wram{}, oam{}, ioRegisters{}, hRam{} {}
-    virtual u8& operator[](u16 address);
 
+    Proxy operator[](u16 address){
+        return Proxy(*this, address);
+    }
+    virtual u8 read(u16 address);
+    virtual void write(u16 address, u8 value);
 
         
 };
--- src/cpu/bitInstructions.cpp
+++ src/cpu/bitInstructions.cpp
@@ -1,14 +1,13 @@
 #include   "cpu.h"
 #include "../types.h"
 
-void CPU::bit_u3_r8(u8 bitPos, u8& value){
+void CPU::bit_u3_r8(u8 bitPos, u8 value){
     bool zero = (value >> bitPos) & 0x1;
     setFlags(zero, 0, 1, Cflag());
 }
 
 void CPU::bit_u3_mHL(u8 bitPos) {
-    u8& value = mem[HL];
-    bit_u3_r8(bitPos, value);
+    bit_u3_r8(bitPos, mem[HL]);
 }
 
 void CPU::res_u3_r8(u8 bitPos, Register& value) {
@@ -16,8 +15,7 @@ void CPU::res_u3_r8(u8 bitPos, Register& value) {
 }
 
 void CPU::res_u3_mHL(u8 bitPos) {
-    u8& value = mem[HL];
-    res_u3_r8(bitPos, value);
+    mem[HL] = static_cast<u8>(~(1<<bitPos) & mem[HL]);
 }
 
 void CPU::rl_r8(u8& value) {
@@ -29,8 +27,11 @@ void CPU::rl_r8(u8& value) {
 }
 
 void CPU::rl_mHL(){
-    u8& value = mem[HL];
-    rl_r8(value);
+    u8 value = mem[HL];
+    bool carry = (value >> 7) & 0x1;
+    bool lowerBit = Cflag();
+    setFlags(value == 0, false, false, carry);
+    mem[HL] = value<<1 | lowerBit;
 }
 
 void CPU::rl_A() {
@@ -45,8 +46,12 @@ void CPU::rlc_r8(u8& value) {
 }
 
 void CPU::rlc_mHL(){
-    u8& value = mem[HL];
-    rlc_r8(value);
+    u8 value = mem[HL];
+    bool carry = (value >>7) & 0x1;
+    value |= carry;
+    setFlags(value == 0, false, false, carry);
+    mem[HL] = value;
+
 }
 
 void CPU::rlc_A() {
@@ -62,8 +67,11 @@ void CPU::rr_r8(u8& value) {
 }
 
 void CPU::rr_mHL(){
-    u8& value = mem[HL];
-    rr_r8(value);
+    u8 value = mem[HL];
+    bool carry = value & 0x1;
+    value = (value >> 1) | (Cflag() << 7);
+    setFlags(value == 0, false, false, carry);
+    mem[HL] = value;
 }
 
 void CPU::rr_A(){
@@ -78,8 +86,11 @@ void CPU::rrc_r8(u8& value){
 }
 
 void CPU::rrc_mHL() {
-    u8& value = mem[HL];
-    rrc_r8(value);
+    u8 value = mem[HL];
+    bool carry = value & 0x1;
+    value = (value >> 1) | (carry << 7);
+    setFlags(value == 0, false, false, carry);
+    mem[HL] = value;
 }
 
 void CPU::rrc_A() {
@@ -92,8 +103,8 @@ void CPU::set_u3_r8(u8 bitPos, u8& value) {
 }
 
 void CPU::set_u3_mHL(u8 bitPos) {
-    u8& value = mem[HL];
-    set_u3_r8(bitPos, value);
+    u8 value = mem[HL];
+    mem[HL] = value | (1 << bitPos);
 }
 
 void CPU::sla_r8(u8& value) {
@@ -103,8 +114,11 @@ void CPU::sla_r8(u8& value) {
 }
 
 void CPU::sla_mHL() {
-    u8& value = mem[HL];
-    sla_r8(value);
+    u8 value = mem[HL];
+    bool carry = (value >> 7) & 0x1;
+    value <<= 1;
+    setFlags(value == 0, false, false, carry);
+    mem[HL] = value;
 }
 
 void CPU::sra_r8(u8& value){
@@ -115,8 +129,11 @@ void CPU::sra_r8(u8& value){
 }
 
 void CPU::sra_mHL() {
-    u8& value = mem[HL];
-    sra_r8(value);
+    u8 value = mem[HL];
+    bool carry = value & 0x1;
+    value = (value >> 1) | (value & 0x80);
+    setFlags(value == 0, false, false, carry);
+    mem[HL] = value;
 }
 
 void CPU::srl_r8(u8& value) {
@@ -126,8 +143,11 @@ void CPU::srl_r8(u8& value) {
 }
 
 void CPU::srl_mHL() {
-    u8& value = mem[HL];
-    srl_r8(value);
+    u8 value = mem[HL];
+    bool carry = value & 0x1;
+    value >>= 1;
+    setFlags(value == 0, false, false, carry);
+    mem[HL] = value;
 }
 
 void CPU::swap_r8(u8& value) {
@@ -136,8 +156,10 @@ void CPU::swap_r8(u8& value) {
 }
 
 void CPU::swap_mHL(){
-    u8& value = mem[HL];
-    swap_r8(value);
+    u8 value = mem[HL];
+    value = (value >> 4) | (value << 4);
+    setFlags(value == 0, false, false, false);
+    mem[HL] = value;
 }
 
 
